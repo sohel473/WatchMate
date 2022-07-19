899a69108786f142c2a7094ad69b7cbab6c7cfd3
Sohel <absohel47@gmail.com>
2022-07-20 04:05:28 +0600

    project initiated

---
+++ app_watchlist/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from app_watchlist.models import Movie
+
+# Register your models here.
+
+admin.site.register(Movie)
---
+++ app_watchlist/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppWatchlistConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app_watchlist'
