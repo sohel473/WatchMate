1258b0d6c06ea6b22a6c6712d4376d735ccd43c3
Sohel <absohel47@gmail.com>
2022-07-31 09:26:33 +0600

    status code implemented

--- app_watchlist/api/views.py
+++ app_watchlist/api/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
+from ast import Try
 from app_watchlist.api.serializers import MovieSerializers
 from app_watchlist.models import Movie
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 @api_view(('GET', "POST"))
 def movie_list(request):
@@ -20,7 +21,11 @@ def movie_list(request):
 
 @api_view(('GET', 'PUT', 'DELETE'))
 def movie_details(request, pk):
-    movie = Movie.objects.get(pk=pk)
+    try:
+        movie = Movie.objects.get(pk=pk)
+    except Movie.DoesNotExist:
+        return Response({"Error": "Movie not found"}, status=status.HTTP_404_NOT_FOUND)
+        
     if request.method == 'GET':
         serializers = MovieSerializers(movie)
         return Response(serializers.data)
@@ -30,7 +35,7 @@ def movie_details(request, pk):
             serializers.save()
             return Response(serializers.data)
         else:
-            return Response(serializers.errors)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
     if request.method == 'DELETE':
         movie.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
