1a0db31177501c2ccb905d8e2f02edb93b960a0b
Abdullah Al Sohel <absohel47@gmail.com>
2022-11-26 16:36:21 +0600

    Disable Browsable Result. Instead use JSON Result

--- WatchMate/settings.py
+++ WatchMate/settings.py
@@ -106,6 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
     # 'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     # 'DEFAULT_PERMISSION_CLASSES': [
     #     'rest_framework.permissions.IsAuthenticated',
