3b7531933b68d4d1f5ec51a9315d0f0a17571767
Abdullah Al Sohel <absohel47@gmail.com>
2022-12-23 12:20:53 +0600

    API Testing - Registration

--- app_user/tests.py
+++ app_user/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
-# Create your tests here.
+
+class RegisterTests(APITestCase):
+
+  def test_register(self):
+    url = reverse('register')
+    data = {
+      'username': 'testuser',
+      'email': 'test@gmail.com',
+      'password': 'testpassword123',
+      'password_confirmation': 'testpassword123'
+    }
+    response = self.client.post(url, data, format='json')
+    
+    self.assertEqual(response.status_code, status.HTTP_201_CREATED)
